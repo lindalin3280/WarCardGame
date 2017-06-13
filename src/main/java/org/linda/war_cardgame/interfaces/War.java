a9bb0c4df1339d17f7d6898f3ec9a0f3fb7579ca
lindalin3280 <lindalin3280 @gmail.com>
2017-06-13 13:44:05 -0400

    add interface war

---
+++ src/main/java/org/linda/war_cardgame/interfaces/War.java
@@ -0,0 +1,31 @@
+package org.linda.war_cardgame.interfaces;
+
+import org.linda.war_cardgame.impl.DeckImpl;
+import org.linda.war_cardgame.pojo.Player;
+
+public interface War {
+	
+	/**
+	 * This method is used to start the game.
+	 * @param suits
+	 * @param ranks
+	 * @param valuesOfRanks
+	 * @param players
+	 */
+	public void startGame(String[] suits, String[] ranks, int[] valuesOfRanks, String[] players);
+
+	/**
+	 * This method is used to distribute cards.
+	 * It will be called only once for each game.
+	 * @param deck
+	 * @param players
+	 * @return
+	 */
+	public Player[] distributeCards(DeckImpl deck, String[] players);
+
+	/**
+	 * 
+	 * @param playersOfWar
+	 */
+	public void playOneRound(Player[] playersOfWar);
+}
